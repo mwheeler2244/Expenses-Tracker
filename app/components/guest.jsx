ee3efffe0c3c7e76cba46406848bc6a266c86f50
Michael  Wheeler <mwheeler2244@yahoo.com>
2025-01-08 20:01:16 -0600

    sign in button

--- app/components/guest.jsx
+++ app/components/guest.jsx
@@ -4,7 +4,6 @@ export default function Guest() {
   return (
     <div className="guest-container">
       <h1 className="guest-title">Please sign in</h1>
-      <SignInButton className="guest-signin-btn" />
     </div>
   );
 }
