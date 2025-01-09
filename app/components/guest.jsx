35e39cb303f9f3e76ef6638105c36c459ff89c25
Michael  Wheeler <mwheeler2244@yahoo.com>
2025-01-08 19:56:51 -0600

    styles

--- app/components/guest.jsx
+++ app/components/guest.jsx
@@ -2,9 +2,9 @@ import { SignInButton } from "@clerk/nextjs";
 
 export default function Guest() {
   return (
-    <div>
-      Please sign in.
-      <SignInButton />
+    <div className="guest-container">
+      <h1 className="guest-title">Please sign in</h1>
+      <SignInButton className="guest-signin-btn" />
     </div>
   );
 }
